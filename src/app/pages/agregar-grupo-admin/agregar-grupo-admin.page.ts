e59fbdbc8ff101a804b4b98a37c9716cf439afbc
Adrián Mora <Adri98.1mm@gmail.com>
2021-10-15 17:43:20 -0600

    course list

--- src/app/pages/agregar-grupo-admin/agregar-grupo-admin.page.ts
+++ src/app/pages/agregar-grupo-admin/agregar-grupo-admin.page.ts
@@ -39,6 +39,7 @@ export class AgregarGrupoAdminPage implements OnInit {
       if (event instanceof NavigationEnd && event.url == '/agregar-grupo-admin') {
         //this.checkIfLoggedIn();
         this.loadTeachers();
+        this.loadCourse();
       }
     });
   }
@@ -49,39 +50,38 @@ export class AgregarGrupoAdminPage implements OnInit {
 
   loadTeachers(){
     this.groupService.getTeachers().subscribe(res => {
-      console.log(res);
-      console.log('asdsd');
       this.Profesores = res[0] as Teacher[];
     });
   }
 
-  loadData(){
+  loadCourse(){
     var recibido = history.state
-    /*if(recibido.curso == undefined)
-      this.router.navigateByUrl('courses-list-admin');*/
-    //else{
-      this.curso.nombre = recibido.curso
+    if(recibido.codigo == undefined)
+      this.router.navigateByUrl('courses-list-admin');
+    else{
+      this.curso.nombre = recibido.nombre
       this.curso.codigo = recibido.codigo
+    }
+  }
 
-      this.sedes = [{sede:'Cartago'}];
-      this.horasInicio =[{hora:"07:00"},{hora:"07:30"},{hora:"08:00"},{hora:"08:30"},{hora:"09:00"},{hora:"09:30"}
+  loadData(){
+    this.sedes = [{sede:'Cartago'}];
+    this.horasInicio =[{hora:"07:00"},{hora:"07:30"},{hora:"08:00"},{hora:"08:30"},{hora:"09:00"},{hora:"09:30"}
+      ,{hora:"10:00"},{hora:"10:30"},{hora:"11:00"},{hora:"11:30"},{hora:"12:00"},{hora:"12:30"},{hora:"13:00"}
+      ,{hora:"13:30"},{hora:"14:00"},{hora:"14:30"},{hora:"15:00"},{hora:"15:30"},{hora:"16:00"},{hora:"16:30"}
+      ,{hora:"17:00"},{hora:"17:30"},{hora:"18:00"},{hora:"18:30"},{hora:"19:00"},{hora:"19:30"},{hora:"20:00"}
+      ,{hora:"20:30"},{hora:"21:00"},{hora:"21:30"}];
+
+    this.horasFin =[{hora:"07:30"},{hora:"08:00"},{hora:"08:30"},{hora:"09:00"},{hora:"09:30"}
       ,{hora:"10:00"},{hora:"10:30"},{hora:"11:00"},{hora:"11:30"},{hora:"12:00"},{hora:"12:30"},{hora:"13:00"}
       ,{hora:"13:30"},{hora:"14:00"},{hora:"14:30"},{hora:"15:00"},{hora:"15:30"},{hora:"16:00"},{hora:"16:30"}
       ,{hora:"17:00"},{hora:"17:30"},{hora:"18:00"},{hora:"18:30"},{hora:"19:00"},{hora:"19:30"},{hora:"20:00"}
       ,{hora:"20:30"},{hora:"21:00"},{hora:"21:30"},{hora:"22:00"}];
-
-      this.horasFin =[{hora:"07:00"},{hora:"07:30"},{hora:"08:00"},{hora:"08:30"},{hora:"09:00"},{hora:"09:30"}
-      ,{hora:"10:00"},{hora:"10:30"},{hora:"11:00"},{hora:"11:30"},{hora:"12:00"},{hora:"12:30"},{hora:"13:00"}
-      ,{hora:"13:30"},{hora:"14:00"},{hora:"14:30"},{hora:"15:00"},{hora:"15:30"},{hora:"16:00"},{hora:"16:30"}
-      ,{hora:"17:00"},{hora:"17:30"},{hora:"18:00"},{hora:"18:30"},{hora:"19:00"},{hora:"19:30"},{hora:"20:00"}
-      ,{hora:"20:30"},{hora:"21:00"},{hora:"21:30"},{hora:"22:00"}];
-      this.dias = [{dia: "L", checked : false}, {dia: "K", checked : false}, {dia: "M", checked : false}
-    ,{dia: "J", checked : false},{dia: "V", checked : false},{dia: "S", checked : false}];
-    //}
+    this.dias = [{dia: "L", checked : false}, {dia: "K", checked : false}, {dia: "M", checked : false}
+      ,{dia: "J", checked : false},{dia: "V", checked : false},{dia: "S", checked : false}];
   }
 
   GetHorasFin(){
-    console.log(this.inicio)
     if (this.inicio == null){
       this.horasFin =[]
     }
@@ -91,7 +91,6 @@ export class AgregarGrupoAdminPage implements OnInit {
       while(seguir){
         if(this.inicio.localeCompare(this.horasFin[0].hora)){
           this.horasFin.shift()
-          console.log(this.horasFin)
           seguir = false
         }
         else{
